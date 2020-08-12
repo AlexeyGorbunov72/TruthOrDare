4d3a7d9f8405db96973ca673196995deb8e6a149
AnyDodic <korri.13@inbox.ru>
2020-08-12 22:51:16 +0300

    Initial Commit

---
+++ TruthOrDare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TruthOrDare
+//
+//  Created by Alexey on 12.08.2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
